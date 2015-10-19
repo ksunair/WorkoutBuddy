cff42326efafcbd1bd3f8808402629869d1c4788
ksunair <ksunair@yahoo.com>
2015-10-18 21:15:25 -0500

    Made navigation and navigation active working.

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,6 +1,8 @@
 Template.navItems.helpers({
     activeIfTemplateIs: function (template) {
         var currentRoute = Router.current();
+        console.log(currentRoute.lookupTemplate());
+        console.log(template);
         return currentRoute &&
         template === currentRoute.lookupTemplate() ? 'active' : '';
     }
