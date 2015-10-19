5b0a528c15196de4fca4e7198ab8211047d894df
ksunair <ksunair@yahoo.com>
2015-10-18 21:36:24 -0500

    dded setup detail panel

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,8 +1,6 @@
 Template.navItems.helpers({
     activeIfTemplateIs: function (template) {
         var currentRoute = Router.current();
-        console.log(currentRoute.lookupTemplate());
-        console.log(template);
         return currentRoute &&
         template === currentRoute.lookupTemplate() ? 'active' : '';
     }
